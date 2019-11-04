1796bb1cc9b4f57c4d31e9e25be4f5b98f45ef5d
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-11-04 03:31:14 -0500

    Update Reducer1B2.py

--- Reducer1B2.py
+++ Reducer1B2.py
@@ -11,19 +11,18 @@ names= defaultdict(list)
 pp=defaultdict(list)
 ply=defaultdict(list)
 for line in sys.stdin:
-    line=line.strip()
+    line=line.strip('\n')
     line=line.split('\t')
     names[line[0]].append(float(line[1]))
 for k,v in names.items():
     pp[k].append(sum(v)/len(v))
-    print'%s\t%s'%(k,pp[k])
-# for k in pp.keys():
-#     name,zone=k.split('_')
-#     ply[name].append([zone,pp[k]])
-# for k,v in ply.items():
-#     for i in v:
-#         if float(i[1][0])>=a:
-#             a=float(i[1][0])
-#             b=i[0]
-#     print '%s\t%s'%(k,b)
+for k in pp.keys():
+    name,zone=k.split('_')
+    ply[name].append([zone,pp[k]])
+for k,v in ply.items():
+    for i in v:
+        if float(i[1][0])>=a:
+            a=float(i[1][0])
+            b=i[0]
+    print '%s\t%s'%(k,b)
 
