b723e192f639e1a6616f8f47d122e323877142a1
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-11-04 02:41:35 -0500

    Add files via upload

---
+++ Reducer1B2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+from collections import defaultdict
+from operator import itemgetter
+names= defaultdict(list)
+pp=defaultdict(list)
+ply=defaultdict(list)
+for line in sys.stdin:
+    line=line.strip('\n')
+    line=line.split('\t')
+    names[line[0]].append(float(line[1]))
+for k,v in names.items():
+    pp[k].append(sum(v)/len(v))
+for k in pp.keys():
+    name,zone=k.split('_')
+    ply[name].append([zone,pp[k]])
+for k,v in ply.items():
+    for i in v:
+        if float(i[1][0])>=a:
+            a=float(i[1][0])
+            b=i[0]
+    print(k,b)
+
---
+++ mapper1B2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+name=['chris paul','james harden','stephen curry','lebron james']
+zone=[]
+for line in sys.stdin:
+    line=line.strip()
+    line=line.split('\t')
+    if (float(line[-10])-float(line[-5])) >=-51.1 and (float(line[-10])-float(line[-5])) <=-3.6:
+        if float(line[-13])>=0 and float(line[-13])<=12:
+            zone.append([line[-2],'Z1',line[-4]])
+        else:
+            zone.append([line[-2],'Z2',line[-4]])
+    elif (float(line[-10])-float(line[-5])) >=-3.5 and (float(line[-10])-float(line[-5])) <=44:
+        if float(line[-13])>=0 and float(line[-13])<=12:
+            zone.append([line[-2],'Z3',line[-4]])
+        else:
+            zone.append([line[-2],'Z4',line[-4]])
+for i in zone:
+        if i[0] in name:
+            print '%s\t%s\n'%(i[0]+'_'+i[1],i[2])
+
