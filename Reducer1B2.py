748e532531bb2610e0a3962b62275898279a8996
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-11-04 04:25:46 -0500

    Update Reducer1B2.py

--- Reducer1B2.py
+++ Reducer1B2.py
@@ -15,15 +15,14 @@ for line in sys.stdin:
     line=line.split('\t')
     names[line[0]].append(float(line[1]))
 for k,v in names.items():
-    pp[k].append(sum(v)/len(v))
-    print '%s\t%s'%(k,pp[k])
-# for k in pp.keys():
-#     name,zone=k.split('_')
-#     ply[name].append([zone,pp[k]])
-# for k,v in ply.items():
-#     for i in v:
-#         if float(i[1][0])>=a:
-#             a=float(i[1][0])
-#             b=i[0]
-#     print '%s\t%s'%(k,b)
+    pp[k]=(sum(v)/len(v))
+for k in pp.keys():
+    name,zone=k.split('_')
+    ply[name].append([zone,pp[k]])
+for k,v in ply.items():
+    for i in v:
+        if float(i[1][0])>=a:
+            a=float(i[1][0])
+            b=i[0]
+    print '%s\t%s'%(k,b)
 
