bc25de40940a2bb281e5f1fd95347d1d25ebb206
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-11-04 02:44:42 -0500

    Create test2B.sh

---
+++ test2B.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+bash /mapreduce-test/start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/input2/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/output2/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab2/input2/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /root/project/shot_F.txt /lab2/input2/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-file ../Project1/mapper1B2.py -mapper ../Project1/mapper1B2.py \
+-file ../Project1/Reducer1B2.py -reducer ../Project1/Reducer1B2.py \
+-input /lab2/input2/* -output /lab2/output2/
+/usr/local/hadoop/bin/hdfs dfs -cat /lab2/output2/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/input2/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/output2/
+bash /mapreduce-test/stop.sh
