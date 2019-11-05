9a6eefea2c6e9910f1404572cd9c47c74695feed
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-11-04 21:08:58 -0500

    Update ReducerB1.py

--- ReducerB1.py
+++ ReducerB1.py
@@ -9,7 +9,7 @@ import sys
 from collections import defaultdict
 dic=defaultdict(list)
 for line in sys.stdin:
-       line = line.strip('\n') ##移除字符的回车 变成了[00:00]5.108.86.176  1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
+       line = line.strip() ##移除字符的回车 变成了[00:00]5.108.86.176  1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
        team,score = line.split('\t')
        player,defender=team.split('_')
         #print(player,defender,score)
