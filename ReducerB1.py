ce2a7c2754dfc31f734e530a50b798c6f05d9fa1
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-11-04 20:47:32 -0500

    Update ReducerB1.py

--- ReducerB1.py
+++ ReducerB1.py
@@ -5,26 +5,27 @@
 # In[ ]:
 
 
-import re
 import sys
 from collections import defaultdict
 dic=defaultdict(list)
 for line in sys.stdin:
-        line = line.strip('\n') ##移除字符的回车 变成了[00:00]5.108.86.176  1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
-        line = line.strip('\t')
-        team,defender,score=line.split('\t')
-        finalscore=score+'_'+defender
-        dic[team].append(finalscore)
+       line = line.strip('\n') ##移除字符的回车 变成了[00:00]5.108.86.176  1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
+       team,score = line.split('\t')
+       player,defender=team.split('_')
+        #print(player,defender,score)
+       finalscore=score+'_'+defender
+        #print(finalscore)
+       dic[player].append(finalscore)
 for i in dic:
         list=[]
         for e in dic[i]:
-            list.append(e)
+            list.append(e.split('_'))
         list.sort(reverse = True)
         max=0
         teamlist=[]
         for w in list:
-            score,defender=w.split('_')
-            score=float(score)
+            defender=w[1]
+            score=float(w[0])
             if score>=max:
                 max=score
                 team=defender
